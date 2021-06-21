2737763420a25d88bc89764948b88d5ee54b03f9
Rocky124252 <connorengelsberg@icloud.com>
2021-06-22 01:55:24 +0800

    removed current source

--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export * from './home';
-export * from './contact';
-export * from './profile';
--- src/sections/index.js
+++
@@ -1,4 +0,0 @@
-export * from './slider';
-export * from './home';
-export * from './contact';
-export * from './profile';
